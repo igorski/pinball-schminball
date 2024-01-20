8b95bff2ad5ba25ac970176d2436388f0c0d2a6c
Igor Zinken <igor.zinken@igorski.nl>
2024-01-20 11:49:13 +0100

    tweaking, need to fix stuck flippers though

--- src/model/physics/engine.ts
+++ src/model/physics/engine.ts
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Igor Zinken 2021-2024 - https://www.igorski.nl
+ * Igor Zinken 2021-2023 - https://www.igorski.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -41,7 +41,7 @@ enum FlipperPositions {
 
 export interface IPhysicsEngine {
     engine: Matter.Engine;
-    update: ( ticks: number, timeScale: number ) => void;
+    update: ( ticks: number ) => void;
     addBody: ( actor: Actor, label: string ) => Matter.Body;
     removeBody: ( body: Matter.Body ) => void;
     updateBodyPosition: ( body: Matter.Body, position: Point ) => void;
@@ -118,8 +118,7 @@ export const createEngine = async (
 
     return {
         engine,
-        update( ticks: number, timeScale: number ): void {
-            engine.timing.timeScale = timeScale;
+        update( ticks: number ): void {
             Matter.Engine.update( engine, ticks );
         },
         addBody( actor: Actor, label: string ): Matter.Body {
